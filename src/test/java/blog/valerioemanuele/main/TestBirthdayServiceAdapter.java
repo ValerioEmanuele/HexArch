9a86d5402f472aa277617c485674938f74abe38b
Valerio Emanuele <valerioemanuele@gmail.com>
2019-04-19 08:37:48 +0200

    Added first integration test

--- src/test/java/blog/valerioemanuele/main/TestBirthdayServiceAdapter.java
+++ src/test/java/blog/valerioemanuele/main/TestBirthdayServiceAdapter.java
@@ -1,22 +1,34 @@
 package blog.valerioemanuele.main;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
+import blog.valerioemanuele.adapter.driven.NotifyConsoleAdapter;
+import blog.valerioemanuele.adapter.driven.UserRepositoryFileAdapter;
+import blog.valerioemanuele.hexagon.drivenport.fornotifyingmessages.Notify;
+import blog.valerioemanuele.hexagon.drivenport.forpersistingdata.UserRepository;
 import blog.valerioemanuele.hexagon.driverport.formanagingbirthdays.BirthdayService;
 
 class TestBirthdayServiceAdapter {
 
-	private final BirthdayService birthdayService;
+	private BirthdayService birthdayService;
 	
-	public TestBirthdayServiceAdapter(BirthdayService birthdayService) {
-		this.birthdayService = birthdayService;
-	}
+	private Class[] constructorArgumentsClass = new Class[] { UserRepository.class, Notify.class };
+	Object[] constructorArguments = new Object[] { new UserRepositoryFileAdapter(), new  NotifyConsoleAdapter() };
+	
 	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test() throws NoSuchMethodException, SecurityException, ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Constructor<?> birthdayServiceImplConst = Class.forName("blog.valerioemanuele.hexagon.internal.BirthdayServiceImpl").getDeclaredConstructor(constructorArgumentsClass);
+		birthdayServiceImplConst.setAccessible(true);
+		birthdayService = (BirthdayService) birthdayServiceImplConst.newInstance(constructorArguments);
+		
+		LocalDate testDate = LocalDate.of(0, 10, 27);
+		birthdayService.wishHappyBirthday(testDate);
 	}
 
 }
